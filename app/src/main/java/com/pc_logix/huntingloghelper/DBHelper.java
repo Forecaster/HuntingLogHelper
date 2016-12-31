089716810d5ee1692e5b50fac5a67ea3894b1cd2
Caitlyn Mainer <michiyo.ravencroft@gmail.com>
2016-12-30 23:29:30 -0600

    Upgrade from version 1 to version 2 of the DB

--- app/src/main/java/com/pc_logix/huntingloghelper/DBHelper.java
+++ app/src/main/java/com/pc_logix/huntingloghelper/DBHelper.java
@@ -14,7 +14,7 @@ public class DBHelper extends SQLiteAssetHelper {
     public SQLiteDatabase DB;
     public String DBPath;
     public static String DBName = "huntinglogs.db";
-    public static final int version = '2';
+    public static final int version = 2;
     public static Context currentContext;
     public static String tableName = "logs";
 
